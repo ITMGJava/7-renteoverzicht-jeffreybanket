eb1f39d3da1dc60b402e6cf8dc0daaf9396c1ae3
jeffreybanket <jeffreybanket@gmail.com>
2020-10-20 20:31:54 +0200

    test renteoverzicht.java helaas lukt de berekening mij niet

---
+++ src/Renteoverzicht2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Renteoverzicht2 {
+    public static void main(String[] args) {
+//We gaan een rentepercentage berekenen aan de hand van je invoer
+        System.out.println("Welkom, Hoe mag ik je noemen?");
+        Scanner s = new Scanner(System.in);
+        String naam = s.nextLine();
+        System.out.println("Welkom, " + naam + "");
+        System.out.println("Met welk bedrag mogen wij je rekening aanvullen");
+System.out.println();
+
+    }
+}
